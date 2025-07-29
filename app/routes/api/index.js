66c93699a3547f0ec0104409db37211450274786
Aligaziz <aligaziznu@gmail.com>
2025-07-29 17:38:04 +0600

    Update index.js

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,4 +1,8 @@
+const bankRoutes = require('./bankRoutes');
+const chatRoutes = require('./chatRoutes');
 module.exports = versionString => ({
 	private: require(`./${versionString}/privateRoutes`),
 	public: require(`./${versionString}/publicRoutes`)
 });
+router.use('/bank', bankRoutes);
+router.use('/chat', chatRoutes);
