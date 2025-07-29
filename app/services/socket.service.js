704431f86c76b6ac02379849e65dca08fe7234e0
Aligaziz <aligaziznu@gmail.com>
2025-07-29 10:04:09 +0600

    Create socket.service.js

---
+++ app/services/socket.service.js
@@ -0,0 +1,47 @@
+const socketio = require('socket.io');
+const jwt = require('jsonwebtoken');
+const { send } = require('#facades/chat');
+const { JWT_SECRET } = require('#configs/jwt');
+
+let io;
+
+function initSocket(server) {
+  io = socketio(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token;
+    if (!token) return next(new Error('Unauthorized'));
+
+    try {
+      const user = jwt.verify(token, JWT_SECRET);
+      socket.user = user;
+      next();
+    } catch (err) {
+      return next(new Error('Invalid token'));
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log(`User ${socket.user.id} connected`);
+
+    socket.on('message', async ({ toUserId, text }) => {
+      const fromUserId = socket.user.id;
+      const message = await send({ fromUserId, toUserId, text });
+
+      // Отправить сообщение себе и получателю (если в сети)
+      socket.emit('message', message);
+      socket.broadcast.emit('message', message); // для простоты без комнат
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`User ${socket.user.id} disconnected`);
+    });
+  });
+}
+
+module.exports = { initSocket };
