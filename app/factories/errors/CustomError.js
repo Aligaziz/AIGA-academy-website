a0eb0a4c34839eaea7bd8ee2d9bf77c9891ef3d1
Aligaziz <aligaziznu@gmail.com>
2025-07-30 10:23:51 +0600

    Update CustomError.js

--- app/factories/errors/CustomError.js
+++ app/factories/errors/CustomError.js
@@ -1,22 +1,40 @@
 class CustomError extends Error {
-	constructor(message) {
-		super(message);
-		this.name = this.constructor.name;
-		this.status = 500;
+  constructor(message = 'Internal Server Error', status = 500, code = null) {
+    super(message);
+    this.name = this.constructor.name;
+    this.status = status; // HTTP статус
+    this.code = code ?? status; // бизнес-код, можно отдельно
+    Error.captureStackTrace(this, this.constructor);
+  }
 
-		// Remove constructor info from stack.
-		Error.captureStackTrace(this, this.constructor);
-	}
+  // Клонирование ошибки
+  replicate(originalError) {
+    this.name = originalError?.name ?? this.name;
+    this.code = originalError?.code ?? this.code;
+    this.status = originalError?.status ?? this.status;
 
-	replicate(originalError) {
-		this.name = originalError?.name ?? this.name;
-		this.code = originalError?.code ?? this.code;
-		this.status = originalError?.status ?? this.status;
-
-		// Append stack from original error.
-		const messageLines =  (this.message.match(/\n/g)||[]).length + 1
-		this.stack = this.stack.split('\n').slice(0, messageLines+1).join('\n') + '\n' + originalError.stack;
-	}
+    const messageLines = (this.message.match(/\n/g) || []).length + 1;
+    this.stack =
+      this.stack.split('\n').slice(0, messageLines + 1).join('\n') +
+      '\n' +
+      originalError.stack;
+  }
 }
 
-module.exports = CustomError;
+class ValidationError extends CustomError {
+  constructor(message = 'Validation failed', code = 400) {
+    super(message, 400, code);
+  }
+}
+
+class UnauthorizedError extends CustomError {
+  constructor(message = 'Unauthorized', code = 401) {
+    super(message, 401, code);
+  }
+}
+
+module.exports = {
+  CustomError,
+  ValidationError,
+  UnauthorizedError
+};
